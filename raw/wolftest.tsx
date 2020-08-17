8fb4b319aac77f97129b035cb32347ab68f817ab
Ben Ferguson <shram@Bens-MacBook-Air.local>
2020-08-17 03:42:12 -0400

    gitignore

--- raw/wolftest.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="wolftest" tilewidth="8" tileheight="8" tilecount="64" columns="8">
- <image source="tileset.png" width="64" height="64"/>
-</tileset>
